5647d43af865a5f1683bb0fa685f97f47a6d08d5
KennethMelendez <kmlndz221@gmail.com.com>
2022-01-23 17:14:41 -0500

    feat: integrate pokemon api
    created a class for making pokemon api calls as well as testing how the
    discord api displays messages
    
    woot

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const http = require('http');
-const Discord = require('discord.js');
-const { Client, Intents} = require('discord.js');
+import fetch from "node-fetch";
+import http from 'http';
+import Discord from 'discord.js';
+import {Client, Intents} from 'discord.js';
+import 'dotenv/config';
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES]});
 const hostname = process.env.HOST_NAME;
 const port = process.env.PORT;
 
-require('dotenv').config();
+
+// dotnav.config();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -25,8 +29,33 @@ server.listen(port, hostname, () => {
     } else if (msg.content == "hi") {
       msg.reply("Wazzzapppp");
     }
+
+    if (msg.content.includes("pokedex #")) {
+        const currentMsg = msg.content;
+        let pokeNo = currentMsg.split("");
+        pokeNo.splice(0,9);
+        pokeNo = pokeNo.join("");
+        console.log(`PokeNo ${pokeNo} called`);
+        const pokemonApi = new PokemonRepositoryApi(pokeNo);
+        pokemonApi.getPokemonData(pokeNo)
+            .then(data => {
+                console.log(data);
+                if(data != null) {
+                    msg.reply(data.sprites.front_default);
+                }
+            });
+    }
+
   })
   
   const mySecret = process.env.TOKEN;
   client.login(mySecret);
 });
+
+class PokemonRepositoryApi {
+
+    getPokemonData(pokeNo){
+        return fetch(`${process.env.POKEMON_API_URL}${pokeNo}`)
+            .then(response => response.json());
+    }
+}
