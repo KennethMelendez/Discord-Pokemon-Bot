79c8c5f65da3b6cc37f354e948f3b721d0a9077c
KennethMelendez <kmlndz221@gmail.com.com>
2022-01-23 22:53:28 -0500

    feat: apply error handling
    bot doesnt crash the server anymore yay

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 import fetch from "node-fetch";
-import http from 'http';
-import {Client, Intents} from 'discord.js';
-import 'dotenv/config';
+import http from "http";
+import { Client, Intents } from "discord.js";
+import "dotenv/config";
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES]});
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 const hostname = process.env.HOST_NAME;
 const port = process.env.PORT;
 
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
   client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
-  })
-  
-  client.on("messageCreate", msg => {
+  });
 
+  client.on("messageCreate", (msg) => {
     console.log(msg.content);
 
     if (msg.content.toLowerCase() == "hello pokedex") {
@@ -29,47 +29,56 @@ server.listen(port, hostname, () => {
     }
 
     if (msg.content.includes("pokedex #")) {
-        const currentMsg = msg.content;
-        let pokeNo = currentMsg.split("");
-        pokeNo.splice(0,9);
-        pokeNo = pokeNo.join("");
-        console.log(`PokeNo ${pokeNo} called`);
-        const pokemonApi = new PokemonRepositoryApi(pokeNo);
+      const currentMsg = msg.content;
+      let pokeNo = currentMsg.split("");
+      pokeNo.splice(0, 9);
+      pokeNo = pokeNo.join("");
+      console.log(`PokeNo ${pokeNo} called`);
 
-        try {
-        pokemonApi.getPokemonData(pokeNo)
-            .then(data => {
-                if(data != null) {
-                    msg.channel.send(
-                    {
-                      content:  `#${pokeNo} ${data.species.name.toUpperCase()}` ,
-                      files: [{
-                        attachment: data.sprites.front_default,
-                        name: `${data.species.name.toUpperCase()}.png`,
-                        description: `#${pokeNo}`
-                      }]
-                    });
-                }
-            });
-        } catch (e) {
-          console.log(e.message());
-        }
+      const pokemonApi = new PokemonRepositoryApi();
+
+      pokemonApi
+        .getPokemonData(pokeNo)
+        .then((data) => {
+          try {
+            if (data != null || data != undefined) {
+              msg.channel.send({
+                content: `#${pokeNo} ${data.species.name.toUpperCase()}`,
+                files: [
+                  {
+                    attachment: data.sprites.front_default,
+                    name: `${data.species.name.toUpperCase()}.png`,
+                    description: `#${pokeNo}`,
+                  },
+                ],
+              });
+            }
+          } catch (e) {
+            msg.channel.send('Pokemon does not exist.');
+            console.log(e.message);
+          }
+        })
+        .catch((error) => {
+          msg.channel.send('Pokemon does not exist.');
+          console.log(error);
+        });
     }
+  });
 
-  })
-  
   const mySecret = process.env.TOKEN;
   client.login(mySecret);
 });
 
 class PokemonRepositoryApi {
+  getPokemonData(pokeNo) {
+    return fetch(`${process.env.POKEMON_API_URL}${pokeNo}`).then((response) => {
+      if (response) {
+        return response.json();
+      }
+    });
+  }
 
-    getPokemonData(pokeNo){
-        return fetch(`${process.env.POKEMON_API_URL}${pokeNo}`)
-            .then(response => response.json());
-    }
-
-    getPokemonSpeciesData(pokeNo){
-        return fetch(`${process.env.POKEMON_API_URL_DESC}${pokeNo}`)
-    }
+  getPokemonSpeciesData(pokeNo) {
+    return fetch(`${process.env.POKEMON_API_URL_DESC}${pokeNo}`);
+  }
 }
